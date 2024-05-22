a6cf7e0153eeea43a41437edbf938725cfaad7f5
gurwls980 <dingdong0907@naver.com>
2024-05-22 17:28:38 +0900

    자바 기초 수업 최초 커밋

---
+++ src/main/java/com/study/java_study/ch02_클래스01/StudentMain.java
@@ -0,0 +1,21 @@
+package com.study.java_study.ch02_클래스01;
+
+public class StudentMain {
+    public static void main(String[] args) {
+
+        /*String name = "손경태";
+        int age = 29;
+        String name2 = "신선애";
+        int age2 = 25;*/
+
+        Student student1 = new Student();
+        Student student2 = new Student();
+
+        student1.name = "손경태";
+        student1.age = 29;
+        student2.name = "신선애";
+        student2.age = 25;
+
+    }
+
+}
