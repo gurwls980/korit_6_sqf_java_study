775ec7c15171553e8bfaa0469305921fec461f47
gurwls980 <dingdong0907@naver.com>
2024-05-23 17:40:42 +0900

    자바 기초 수업 최초 커밋

---
+++ src/main/java/com/study/java_study/ch04_제어/StringUtils.java
@@ -0,0 +1,13 @@
+package com.study.java_study.ch04_제어;
+
+public class StringUtils {
+
+    boolean isEmpty(String str) {
+        if(str == null) {
+            return true;
+        }
+        return str.equals("");
+        //return str == null || str.isEmpty();
+    }
+
+}
