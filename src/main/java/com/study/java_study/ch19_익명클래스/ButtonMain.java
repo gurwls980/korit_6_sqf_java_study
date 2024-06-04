46ad210099e8d9d59b0c7e003400c2ffa8e0912c
gurwls980 <dingdong0907@naver.com>
2024-06-04 17:15:10 +0900

    0604 수업

---
+++ src/main/java/com/study/java_study/ch19_익명클래스/ButtonMain.java
@@ -0,0 +1,31 @@
+package com.study.java_study.ch19_익명클래스;
+
+public class ButtonMain {
+    public static void main(String[] args) {
+//        Button homeButton = new HomeButton();
+//        Button loginButton = new Button() {
+//            @Override
+//            public void onclick() {
+//                System.out.println("로그인 하기");
+//            }
+//        };
+//
+//        click(homeButton);
+//        click(loginButton);
+
+        click(new HomeButton());
+
+        click(new Button() {
+            @Override
+            public void onclick() {
+                System.out.println("로그인 하기");
+            }
+        });
+
+    }
+
+    public static void click(Button button) {
+        System.out.println(button.getClass());
+        button.onclick();
+    }
+}
